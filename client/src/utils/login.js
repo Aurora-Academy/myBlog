c77e7b4fefac3dd90298d351ad3c9b1b56dc623f
Raktim Shrestha <rakimsth@gmail.com>
2024-06-06 19:32:41 +0545

    fixed random access_token issue

--- client/src/utils/login.js
+++ client/src/utils/login.js
@@ -4,23 +4,31 @@ import { jwtDecode } from "jwt-decode";
 import { getToken } from "./token";
 
 export const isLoggedIn = () => {
-  const token = getToken();
-  if (!token) return false;
-  const { exp } = jwtDecode(token);
-  const now = moment(new Date().valueOf());
-  const expDate = moment.unix(exp);
-  if (now < expDate) return true;
-  return false;
+  try {
+    const token = getToken();
+    if (!token) return false;
+    const { exp } = jwtDecode(token);
+    const now = moment(new Date().valueOf());
+    const expDate = moment.unix(exp);
+    if (now < expDate) return true;
+    return false;
+  } catch (e) {
+    return false;
+  }
 };
 
 export const isValidRoles = (roles = []) => {
-  if (roles === "") return true;
-  const token = getToken();
-  if (!token) return false;
-  const { data: user } = jwtDecode(token);
-  const isValid = roles.some((r) => user.role.includes(r));
-  if (!isValid) return false;
-  return true;
+  try {
+    if (roles === "") return true;
+    const token = getToken();
+    if (!token) return false;
+    const { data: user } = jwtDecode(token);
+    const isValid = roles.some((r) => user.role.includes(r));
+    if (!isValid) return false;
+    return true;
+  } catch (e) {
+    return false;
+  }
 };
 
 export const setCurrentUser = () => {
